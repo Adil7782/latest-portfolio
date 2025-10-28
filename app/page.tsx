d3a6815e05ae36257e8c80164b54fd5383196da3
Adil <adilsaaly@gmail.com>
2025-10-29 01:10:41 +0530

    gsc

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,7 @@ import { ContactSection } from '@/components/contact-section';
 import { Footer } from '@/components/footer';
 
 export default function Home() {
+  console.log("first")
   return (
     <main className="min-h-screen">
       <Navbar />
