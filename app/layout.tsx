8d961f5610cab68204208073793c97060360e685
Adil <adilsaaly@gmail.com>
2025-10-29 09:32:17 +0530

    gsc verifi

--- app/layout.tsx
+++ app/layout.tsx
@@ -46,6 +46,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <meta name="google-site-verification" content="o1Q4_ZNzeoIu8rRbz3n5NVkFJzqEsf7CvQoCQoz0byk" />
+      </head>
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           {children}
